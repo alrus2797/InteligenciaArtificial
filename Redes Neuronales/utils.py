676eafdd614c13dabb4c4a5fe965f15e8fbe33fe
alrus2797 <alrus2797@gmail.com>
2019-12-18 16:40:39 -0500

    Convert arrays to numpy arrays for utils

--- Redes Neuronales/utils.py
+++ Redes Neuronales/utils.py
@@ -22,6 +22,8 @@ def plot_confusion_matrix(df_confusion, title='Confussion Matrix', cmap=plt.cm.h
 /** verif_res   : Obtained class
 '''
 def make_confussion_matrix(verif_target,verif_res, title = 'Confussion Matrix', names = {}, verbose= False):
+    verif_target = np.array(verif_target)
+    verif_res = np.array(verif_res)
     confussion_matrix = pd.crosstab(verif_target, verif_res, rownames=['Actual'], colnames=['Predicted'], margins=False, normalize= True, dropna=False)
     confussion_matrix =  confussion_matrix.rename(columns=names, index=names)
     if verbose:
